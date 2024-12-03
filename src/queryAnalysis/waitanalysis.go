e50f31ac929327f1d2b15278b98dedd75500f92b
avadla <avadla@newrelic.com>
2024-12-03 10:47:58 +0530

    Refactor code

--- src/queryAnalysis/waitanalysis.go
+++
@@ -1,9 +0,0 @@
-package queryAnalysis
-
-import "fmt"
-
-// AnalyzeWaits performs wait analysis of queries
-func AnalyzeWaits() {
-	// Add logic for wait analysis
-	fmt.Println("Performing wait analysis of queries...")
-}
